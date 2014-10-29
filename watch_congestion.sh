f49a1370f5226d5f53ad5a6b8d3cfa633f25dc61
Eric Covener <covener@gmail.com>
2014-10-29 15:08:33 -0400

    avoid variable length output in rtt

--- watch_congestion.sh
+++ watch_congestion.sh
@@ -12,10 +12,11 @@ fi
 while true; do 
   sleep .05 
   SSOUT=$(ss -teino state ESTABLISHED | grep -A1 "$1")
-  CWND=$(echo $SSOUT | grep cwnd: | sed -r -e 's/.*cwnd:([0-9]+).*/\1/g')
-  RTT=$(echo $SSOUT | grep rtt: | sed -r -e 's/.*rtt:([0-9]+).*/\1/g')
-  if [ ! -z "$CWND" ]; then
-    printf "cwnd=$CWND rtt=$RTT, approx kb given 1500 MSS $(($CWND * 1500 / 1024))kb\n";
+  CWNDETC=$(echo $SSOUT | grep cwnd: | sed -r -e 's/.*(rto.*) cwnd:([0-9]+) .*/\2 \1/g')
+  CWND=$(echo $SSOUT | grep cwnd: | sed -r -e 's/.*(rto.*) cwnd:([0-9]+) .*/\2/g')
+  CWNDETC=`echo $CWNDETC|sed -r -e 's/(rtt:[0-9]+)(\S)*/\1/g'`
+  if [ ! -z "$CWNDETC" ]; then
+    printf "$CWNDETC ;  approx kb given 1500 MSS $(($CWND * 1500 / 1024))kb\n";
   fi
 done
 
