12bff9770742c6da7cd446f7ccd08a454a285371
Eric Covener <covener@gmail.com>
2014-10-29 12:19:18 -0400

    track rtt too

--- watch_congestion.sh
+++ watch_congestion.sh
@@ -5,14 +5,17 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-set -e 
+/sbin/ip route
+/sbin/sysctl -a|grep congest
+/sbin/ifconfig -a
 
-ip route
 while true; do 
   sleep .05 
-  CWND=$(ss -teino state ESTABLISHED | grep -A1 "$1" | grep cwnd: | sed -r -e 's/.*cwnd:([0-9]+).*/\1/g')
+  SSOUT=$(ss -teino state ESTABLISHED | grep -A1 "$1")
+  CWND=$(echo $SSOUT | grep cwnd: | sed -r -e 's/.*cwnd:([0-9]+).*/\1/g')
+  RTT=$(echo $SSOUT | grep rtt: | sed -r -e 's/.*rtt:([0-9]+).*/\1/g')
   if [ ! -z "$CWND" ]; then
-    printf "cwnd=$CWND, approx kb given 1500 MSS $(($CWND * 1500 / 1024))kb\n";
+    printf "cwnd=$CWND rtt=$RTT, approx kb given 1500 MSS $(($CWND * 1500 / 1024))kb\n";
   fi
 done
 
