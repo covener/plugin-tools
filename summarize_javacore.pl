84ab615d646a60ea25446b2d57f9f4753f760013
Eric Covener <covener@gmail.com>
2015-02-13 09:46:35 -0500

    very basic tool for summarizing a javacore

---
+++ summarize_javacore.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+# summarize_javacore.pl /path/to/javacore | sort | uniq -c | sort -n
+
+my $state = "stackstart";
+
+while(<>) {
+  if ($state eq "stackstart") { 
+    if ($_ =~ m/3XMTHREADINFO3.*Java/) { 
+        $state = "feed";
+        next;
+    }
+  }
+  elsif ($state eq "feed") { 
+    if ($_ !~ m/4XESTACKTRACE/) { 
+        $state = "print";
+        next;
+      }
+      chomp();
+      if (m/^.* at (.+?)\(/) { 
+        my $fn = $1;
+        $fn =~ s@^java/.*?([^/]+)$@\1@g;
+        $fn =~ s@sun/misc/(.+)@\1@g;
+        $fn =~ s@concurrent/locks/(.+)@\1@g;
+        $fn =~ s@concurrent/(.+)@\1@g;
+        if ($stack eq "") { 
+            $stack = "$fn";
+        }
+        else { 
+            $stack = "$stack<$fn";
+        }
+        next;
+      }
+      die($_);
+  }
+  elsif ($state eq "print") { 
+    print $stack . "\n\n";
+    $stack = "";
+    $state = "stackstart";
+  }
+  else { 
+    die ("what is state $state");
+  }
+}
+  
