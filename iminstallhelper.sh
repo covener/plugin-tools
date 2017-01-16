db8120e5422e53aaf1a6418e860ccf73b32623bb
Eric Covener <covener@gmail.com>
2017-01-16 15:24:48 -0500

    refactor and change args
    make the first parameter an action
    
    minor simplifications throughout

--- iminstallhelper.sh
+++ iminstallhelper.sh
@@ -3,38 +3,35 @@
 # covener's script to list/install/patch whatever is found 
 # in an IM repository with an global or unzipped IIM.
 
-# Prerequisites to using this script.  
-#  1) unpack IIM for this platform in ~/iim
-#  3) If you use e-images, you must unzip diskX for an offering and pass the unzipped dir as the argument
-
 usage() { 
-  echo "$0 [-g] -r /path/to/driverdownload|URL to repository  [ -i /path/to/installroot|-u /path/to/install-to-update ] -p offering-or-fix-to-install|\"list\""
-  printf "\n\nThis script helps you install and update an IM-based WAS product using online repositories or zips (repos or interim fixes). it can use a global or unzipped IIM\n"
-  printf "\tInstall\n"
-  printf "\t\t List packages: $0 -r your-repo -p list -i ~/ihs8557\n"
-  printf "\t\t Install: $0 -r your-repo -p com.ibm.websphere.IHS.v85_8.5.5007.20150820_2140  -i ~/ihs8557\n"
-  printf "\t\t\t V9 hint: Must select a JDK for install like: -p \"com.ibm.websphere.IHSILAN.v90 com.ibm.java.jdk.v8\"\n\n"
-  printf "\tUpdate to fixpack\n"
-  printf "\t\tList Contents: $0 -r your-repo -p list -u ~/ihs8557\n"
-  printf "\t\tInstall Contents: $0 -r your-repo -p com.ibm.websphere.IHS.v85_8.5.5007.20150820_2140 -u ~/ihs8557\n\n"
-  printf "\tAdd IFIX\n"
-  printf "\t\tList contents:  $0 -r ~/8.5.5.7-WS-WASIHS-OS390-IFPI70372.zip -u ~/ihs8557\n"
-  printf "\t\tInstall contents: $0 -r ~/8.5.5.7-WS-WASIHS-OS390-IFPI70372.zip -u  ~/ihs8557 -p 8.5.5.7-WS-WASIHS-OS390-IFPI70372_8.5.5007.20161010_1459\n"
-  printf "\tUninstall: $0 -x /installroot \n"
-
-  printf "\nOptions:\n" 
+  echo "$0 list|install|uninstall|update|install-im|install-local-im  [-l] -r some-repo [-i install-root]  -p offering|\"list\""
+  printf "\nList available packages to install:"
+  printf "\n\t$0 list -r /path/to/driverdownload|URL to repository"
+  printf "\n\nInstall package:"
+  printf "\n\t$0 install -r /path/to/driverdownload|URL -p com.ibm.... -i /opt/InstallRoot"
+  printf "\n\nInstall fixpack (same as full install):"
+  printf "\n\t$0 install -r /path/to/driverdownload|URL -p com.ibm.... -i /opt/InstallRoot"
+  printf "\n\nApply IFIX:"
+  printf "\n\t$0 update -r /path/to/ifix.zip -i /opt/InstallRoot"
+  printf "\n\nRemove last IFIX:"
+  printf "\n\t$0 uninstall -i /opt/InstallRoot"
+  printf "\n\nInstall global IIM:"
+  printf "\n\t$0 install-im"
+  printf "\n\nOptions:\n" 
   printf "\t -r specifies a repo -- zip or http/https\n"
   printf "\t -U/P are user/pass for online repos. You should be prompted w/o these\n"
   printf "\t -t forces a temporary IIM in the -i dir\n"
   exit 1
 }
 
-if [ $# -lt 1 ]; then
+ACTION=$1;
+shift
+
+if [ $# -lt 1  -a $ACTION != "install-im" ]; then
   usage
 fi
 
-# Change to zero to avoid global IIM instal.
-
+# Look for a global IIM install by default.
 GLOBAL=1
 # No configuration below!
 
@@ -47,33 +44,28 @@ if [ -x /opt/csw/bin/wget ]; then
   PATH=$PATH:/opt/csw/bin
 fi
 PKGS=list
-INSTDIR=$IIMDL
 
 NEED_AUTH=0
-while getopts "gti:r:p:u:U:P:x:" flag
+while getopts "lti:r:p:U:P:x:" flag
 do
   case $flag in
     i) INSTDIR=$OPTARG ;;
-    u) UPDATEINST=$OPTARG 
-       INSTDIR=$UPDATEINST;;
     r) PKGDL=$OPTARG ;;
     p) PKGS=$OPTARG ;;
     U) REPOUSER=$OPTARG ; NEED_AUTH=1 ;;
     P) PASS=$OPTARG ;;
-    g) GLOBAL=1;;
-    t) TEMPIM=1;;
-    x) UNINSTALL=$OPTARG;;
- 
+    l) GLOBAL=0;;
   esac
 done
 
-if [ ! "$PKGS" = "list" -a -z "$INSTDIR" ]; then
-  if [ -z $UNINSTALL ]; then
-    echo "One of -i or -u is required"
-    exit 1
-  fi
+if [ $ACTION = "install" -o $ACTION = "update" -o $ACTION = "uninstall" ]; then
+    if [ -z "$INSTDIR" ]; then
+      echo "$0: -i install-path is required for install, update, or uninstall"
+      exit 1
+    fi
 fi
 
+# Find the global IM unless -l was forced
 if [ x"$GLOBAL" = "x1" ]; then
     POSSIBLE_GLOBAL_IMS="/c/opt/Moonstone/IM /opt/IM /opt/IBM/InstallationManager /opt/Moonstone/InstallationManager /opt/Moonstone/IM"
     for PIM in $POSSIBLE_GLOBAL_IMS; do
@@ -95,64 +87,15 @@ fi
 STORAGE=$HOME/iim.storage
 MASTER=$HOME/iim.password
 
+# Override these for the local IM scenario
 IMDATA=""
 IMSHARED=""
 if [ $GLOBAL -eq 0 ]; then
   IMDATA="$IIMDL/IM-data"
-  IMSHARED=$IIMDL/IM-shared
+  IMSHARED=$"IIMDL/IM-shared"
 fi
 
-if [ ! -d "$IIMDL" ]; then
-    # Borrowed from ihsinstall.sh
-    echo "no $IIMDL or global IM detected. Installing 185 via 160..."
-    # We need a global IIM. Grab the 1.6.0 full agent and use the raw kit to install 1.8.5
-    OS=`uname -s`
-    if [ ! -f /tmp/iimold.zip ]; then
-      case $OS in
-        AIX) wget -q ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/16/zips/agent.installer.aix.motif.ppc_1.6.0.20120831_1216.zip -O /tmp/iimold.zip
-             ;;
-        Linux) wget -q ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/16/zips/agent.installer.linux.gtk.x86_64_1.6.0.20120831_1216.zip -O /tmp/iimold.zip
-             ;;
-      esac
-    fi 
-    mkdir /tmp/iimold
-    (cd /tmp/iimold && unzip /tmp/iimold.zip)
-
-    # Use the unpacked IIM zip to install 1.8.5 from the repo.
-    if [ -z "$TEMPIM" ]; then
-      /tmp/iimold/tools/imcl install com.ibm.cic.agent -acceptLicense  -repositories ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/185/repository/
-    else
-      if [ -f /etc/.ibm/registry/InstallationManager.dat ]; then
-          echo "You seem to have a global IIM but asked for a scratch/temp IIM.  This doesn't work.  Hide/restore /etc/.ibm/registry/InstallationManager.dat?"
-          exit 1
-      fi
-      /tmp/iimold/tools/imcl install com.ibm.cic.agent -acceptLicense  -repositories ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/185/repository/ \
-            -installationDirectory $INSTDIR -dataLocation $IMDATA
-    fi
-    IIMDL=/opt/IBM/InstallationManager
-    IMCL=$IIMDL/eclipse/tools/imcl
-    IMUTILSC=$IIMDL/eclipse/tools/imutilsc
-    GLOBAL=1
-fi
-
-IMCL_LOCAL=$IIMDL/imcl.local
-if [ $GLOBAL = "1" ]; then
-  # No need for a local
-  IMCL_LOCAL=$IMCL
-fi
-
-if [ -d /cygdrive ]; then
-  INSTPREFIX=c:/inst
-else
-  INSTPREFIX=$HOME/inst
-fi
-
-if [ -z "$PKGS" ]; then
-  echo "Assuming -p list"
-  PKGS=list
-fi
-
-
+# Setup parameters for real command-line IM invocations
 if [ -d /cygdrive ]; then
   if [ $GLOBAL -eq 0 ]; then
     IMDATA_NATIVE=`cygpath -m $IMDATA`
@@ -170,10 +113,6 @@ else
   IMSHARED_NATIVE=$IMSHARED
 fi
 
-if [ ! -w "$STORAGE_NATIVE" ]; then
-  echo "Warning, $STORAGE_NATIVE is not writable. Passwords provided interactively will not be saved"
-fi
-
 IMDATA_ARG=""
 IMSHARED_ARG=""
 
@@ -183,33 +122,7 @@ if [ $GLOBAL -eq 0 ]; then
   IMSHARED_ARG="-sharedDataLocation $IMDATA_NATIVE"
 fi
 
-if [ ! -d "$PKGDL" -a ! -f "$PKGDL" ]; then
-  OUT=`wget --no-check-certificate "$PKGDL" 2>&1`
-  # modern wget 
-  if [ $? -eq 6 ]; then
-    NEED_AUTH=1
-  fi
-  # ancient wget 
-  if echo "$OUT" | grep "401" >/dev/null; then
-    NEED_AUTH=1
-  fi
-else
-  # IIM wont take a relative path.
-  FC=`echo $PKGDL|sed -e 's/^\(.\).*/\1/'`
-  if [ ! $FC = "/" ]; then
-    PKGDL=$PWD/$PKGDL
-  fi
-fi
-
-
-if [ $NEED_AUTH -eq 1 -a ! -f "$STORAGE" ]; then
-  echo "No $STORAGE if your repo is GSA, stash a PW in ~/iim.password and run e.g. \n\n\t $IMUTILSC  saveCredential -url $PKGDL -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE -userName youruser -userPassword yourpass" 
-  exit 1
-fi
-
-if [ $NEED_AUTH -eq 1 -a -n "$REPOUSER" ]; then
- $IMUTILSC saveCredential -url $PKGDL -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE -userName $REPOUSER 
-fi
+# IM functions
 
 listAvailablePackages() { 
   echo "  Determing available packages in $PKGDL..."
@@ -219,8 +132,8 @@ listAvailablePackages() {
 }
 
 lisInstalledPackages() { 
-  echo "  Determing installed packages in $UPDATEINST..."
-  PACKAGE=`$IMCL listInstalledPackages  -installationDirectory $UPDATEINST $IMDATA_ARG | grep com.ibm`
+  echo "  Determing installed packages in $INSTDIR..."
+  PACKAGE=`$IMCL listInstalledPackages  -installationDirectory $INSTDIR $IMDATA_ARG | grep com.ibm`
 }
 
 listAvailableFixes() { 
@@ -235,42 +148,127 @@ listAvailableFixes() {
 }
 
 installFix() { 
-  echo "  Installing $PKGS from $PKGDL into $UPDATEINST..."
+  echo "  Installing $PKGS from $PKGDL into $INSTDIR ..."
   if [ -d /cygdrive ]; then
-    $UPDATEINST/bin/versionInfo.bat -ifixes
+    $INSTDIR/bin/versionInfo.bat -ifixes
   else
-    $UPDATEINST/bin/versionInfo.sh -ifixes
+    $INSTDIR/bin/versionInfo.sh -ifixes
   fi
 
   $IMCL install $PKGS -repositories $PKGDL                   \
         $IMDATA_ARG \
-        -installationDirectory "$UPDATEINST"                    \
+        -installationDirectory "$INSTDIR"                    \
         -acceptLicense \
         -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE
 
   if [ -d /cygdrive ]; then
-    $UPDATEINST/bin/versionInfo.bat -ifixes
+    $INSTDIR/bin/versionInfo.bat -ifixes
   else
-    $UPDATEINST/bin/versionInfo.sh -ifixes
+    $INSTDIR/bin/versionInfo.sh -ifixes
   fi
 
 }
+checkRepoAuth() { 
+  if [ ! -d "$PKGDL" -a ! -f "$PKGDL" ]; then
+    OUT=`wget --no-check-certificate "$PKGDL" 2>&1`
+    # modern wget 
+    if [ $? -eq 6 ]; then
+      NEED_AUTH=1
+    fi
+    # ancient wget 
+    if echo "$OUT" | grep "401" >/dev/null; then
+      NEED_AUTH=1
+    fi
+  else
+    # IIM wont take a relative path.
+    FC=`echo $PKGDL|sed -e 's/^\(.\).*/\1/'`
+    if [ ! $FC = "/" ]; then
+      PKGDL=$PWD/$PKGDL
+    fi
+  fi
+  
+  
+  if [ $NEED_AUTH -eq 1 -a ! -f "$STORAGE" ]; then
+    echo "No $STORAGE if your repo is GSA, stash a PW in ~/iim.password and run e.g. \n\n" 
+    echo "\t $IMUTILSC  saveCredential -url $PKGDL -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE -userName youruser -userPassword yourpass" 
+    exit 1
+  fi
+  
+  if [ $NEED_AUTH -eq 1 -a ! -w "$STORAGE_NATIVE" ]; then
+    echo "Warning, $STORAGE_NATIVE is not writable. Passwords provided interactively will not be saved"
+  fi
+  
+  if [ $NEED_AUTH -eq 1 -a -n "$REPOUSER" ]; then
+   $IMUTILSC saveCredential -url $PKGDL -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE -userName $REPOUSER 
+  fi
+}
 
 
-ARCH=`uname -m`
-OPKGS=$PKGS
+if [ $ACTION = "list" ] ; then
+  listAvailablePackages
+  exit 0
+fi
 
-if [ -n "$UNINSTALL" ]; then
-    set -x
-    UPDATEINST=$UNINSTALL
-    lisInstalledPackages
-    $IMCL $IMDATA_ARG uninstall $PACKAGE -installationDirectory $UNINSTALL 
-    set +x
-    exit 
+if [ $ACTION = "install-im" ]; then
+    echo "Trying to install global IIM"
+    if [ -d "/opt/IBM/InstallationManager" ]; then
+      echo "/opt/IBM/InstallationManager exists"
+      exit 1
+    fi
+    # We need a global IIM. Grab the 1.6.0 full agent and use the raw kit to install 1.8.5
+    OS=`uname -s`
+    rm -f /tmp/iimold.zip
+    if [ ! -f /tmp/iimold.zip ]; then
+      echo "Downloading old IIM to boostrap..."
+      case $OS in
+        AIX) wget -q ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/16/zips/agent.installer.aix.motif.ppc_1.6.0.20120831_1216.zip -O /tmp/iimold.zip
+             ;;
+        Linux) wget -q ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/16/zips/agent.installer.linux.gtk.x86_64_1.6.0.20120831_1216.zip -O /tmp/iimold.zip
+             ;;
+      esac
+    fi 
+    mkdir /tmp/iimold
+    (cd /tmp/iimold && unzip /tmp/iimold.zip)
+
+    # Use the unpacked IIM zip to install 1.8.5 from the repo.
+    /tmp/iimold/tools/imcl install com.ibm.cic.agent -acceptLicense  -repositories ftp://public.dhe.ibm.com/software/rationalsdp/v7/im/185/repository/
+
+    IIMDL=/opt/IBM/InstallationManager
+    IMCL=$IIMDL/eclipse/tools/imcl
+    IMUTILSC=$IIMDL/eclipse/tools/imutilsc
+    exit  0
+fi
+if [ $ACTION = "install-local-im" ]; then
+  echo "install-local-im not yet implemented"
+  exit 1
+fi
+
+if [ -d /cygdrive ]; then
+  INSTPREFIX=c:/inst
+else
+  INSTPREFIX=$HOME/inst
+fi
+
+if [ -z "$PKGS" ]; then
+  echo "Assuming -p list"
+  PKGS=list
 fi
 
 
-if [ -n "$UPDATEINST" ];  then
+checkRepoAuth
+ARCH=`uname -m`
+OPKGS=$PKGS
+
+if [ $ACTION = "uninstall" ]; then
+    set -x
+    INSTDIR=$UNINSTALL
+    lisInstalledPackages
+    $IMCL $IMDATA_ARG uninstall $PACKAGE -installationDirectory $UNINSTALL 
+    set +x
+    exit 0
+fi
+
+if [ $ACTION = "update" ];  then
   # Return value in PACKAGES
   lisInstalledPackages 
   # Return value in FIXES 
@@ -279,7 +277,7 @@ if [ -n "$UPDATEINST" ];  then
   if [ x"${OPKGS}" = x"list" ]; then
     echo "Found Fixes: "
     echo "  $FIXES"
-    echo "Hint: $0 $@ -p $FIXES"
+    echo "Hint: $0 update $@ -p $FIXES"
     exit 0
   fi
   if [ x"${OPKGS}" = x"all" ]; then
@@ -292,84 +290,52 @@ if [ -n "$UPDATEINST" ];  then
   exit 0
 
 fi
-
-if echo "$PKGS"|xargs | grep credentials 2>&1 >/dev/null; then
-  echo "No Credentials for $PKGDL"
-  echo "$IIMDL/tools/imutilsc  saveCredential -url $PKGDL -userName youruser -userPassword yourpass -secureStorageFile ~/iim.storage  -masterPasswordFile ~/iim.password"
-  exit 1
-fi
-
-
-if [ x"${OPKGS}" = x"list" ]; then
-   listAvailablePackages
-   for PKG in $PKGS; do
-     printf --  "$PKG\n"
-   done
-   exit 1
-fi
-
-if echo $PKGS | grep v9 ; then
-  if [ -n "$UPDATEINST" ]; then
-      ACTUALINST=$UPDATEINST
-  elif [ -n "$INSTDIR" ]; then
-      ACTUALINST=$INSTDIR
-  else
-    # first package to install
-    SUB=`echo $PKGS|awk '{print $1}'`
-    ACTUALINST="$HOME/inst/$SUB"
-  fi
-if echo $PKGS | grep EDGE >/dev/null; then
-        $IMCL install $PKGS -repositories $PKGDL  \
-          $IMDATA_ARG \
-          -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
-          -showProgress                                     
-else 
-  $IMCL install $PKGS                                  \
-      -repositories $PKGDL                             \
-      -installationDirectory   "$ACTUALINST"           \
-      -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE    \
-          $IMDATA_ARG \
-      -showProgress                                     
-fi
-
-else
-    # 8.0/8.5
-    for PKG in $PKGS; do
-      if [ -n "$INSTDIR" ]; then
-        ACTUALINST=$INSTDIR
-      elif [ -n "$UPDATEINST" ]; then
-          ACTUALINST=$UPDATEINST
-      else
+if [ $ACTION = "install" ]; then
+    if echo $PKGS | grep v9 ; then
       # first package to install
-        SUB=`echo $PKGS|awk '{print $1}'`
-        ACTUALINST="$INSTDIR/$SUB"
+      SUB=`echo $PKGS|awk '{print $1}'`
+      if echo $PKGS | grep EDGE >/dev/null; then
+              $IMCL install $PKGS -repositories $PKGDL  \
+              $IMDATA_ARG \
+                -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
+                -showProgress                                     
+      else 
+        $IMCL install $PKGS                                  \
+            -repositories $PKGDL                             \
+            -installationDirectory   "$INSTDIR"           \
+            -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE    \
+                $IMDATA_ARG \
+            -showProgress                                     
       fi
-  
-      PROPS=""
-      case $PKG in 
-        *APPCLIENT*) PROPS="user.wasjava=java8,user.appclient.serverHostname=localhost,user.appclient.serverPort=2809";;
-        *IHS*) PROPS="user.wasjava=java6,user.ihs.httpPort=80,user.ihs.allowNonRootSilentInstall=true";;
-        *WAS*) PROPS="user.wasjava=java8;;"
-      esac
-      if echo $PKG | grep EDGE >/dev/null; then
-        $IMCL install $PKG -repositories $PKGDL  \
-          $IMDATA_ARG \
-          -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
-          -showProgress                                     
-      elif [ -n "$PROPS" ]; then
-        $IMCL install $PKG -repositories $PKGDL  \
-          $IMDATA_ARG \
-          -installationDirectory   "$ACTUALINST"            \
-          -properties "$PROPS"                              \
-          -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
-          -showProgress                                     
-      else
-        $IMCL install $PKG -repositories $PKGDL                                                   \
-          $IMDATA_ARG \
-          -installationDirectory   "$ACTUALINST"                                                  \
-          -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE   \
-          -showProgress                                     
-      fi
-    done
+    else
+        # 8.0/8.5
+        for PKG in $PKGS; do
+          PROPS=""
+          case $PKG in 
+            *APPCLIENT*) PROPS="user.wasjava=java8,user.appclient.serverHostname=localhost,user.appclient.serverPort=2809";;
+            *IHS*) PROPS="user.wasjava=java6,user.ihs.httpPort=80,user.ihs.allowNonRootSilentInstall=true";;
+            *WAS*) PROPS="user.wasjava=java8;;"
+          esac
+          if echo $PKG | grep EDGE >/dev/null; then
+            $IMCL install $PKG -repositories $PKGDL  \
+              $IMDATA_ARG \
+              -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
+              -showProgress                                     
+          elif [ -n "$PROPS" ]; then
+            $IMCL install $PKG -repositories $PKGDL  \
+              $IMDATA_ARG \
+              -installationDirectory   "$INSTDIR"            \
+              -properties "$PROPS"                              \
+              -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE                                   \
+              -showProgress                                     
+          else
+            $IMCL install $PKG -repositories $PKGDL                                                   \
+              $IMDATA_ARG \
+              -installationDirectory   "$INSTDIR"                                                  \
+              -acceptLicense -secureStorageFile $STORAGE_NATIVE  -masterPasswordFile $MASTER_NATIVE   \
+              -showProgress                                     
+          fi
+        done
+    fi
 fi
 
